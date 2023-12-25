6a9628c3490957d59a59a5ce03b070388eedc705
nooshinbarazi <nooshinbry@gmail.com>
2023-12-25 16:43:47 +0330

    Providing the Store

---
+++ redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+
+export const store = configureStore({
+    reducer: {},
+  })
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
