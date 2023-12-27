ac70ff2b9c7a5c7fad16559b2e415ebb833a4c9a
nooshinbarazi <nooshinbry@gmail.com>
2023-12-27 19:09:51 +0330

    feat: dispatch fetchfoods from store

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -4,25 +4,28 @@ import MenuList from "./components/MenuList";
 import { MenuCard } from "./components/MenuCard";
 import { ImageSlider } from "./components/ImageSlider/ImageSlider";
 import { FoodList } from "./components/FoodList/FoodList";
+import { useAppDispatch, useAppSelector } from "@/redux/store";
+import { getFoods } from "@/redux/features/foods/foodsSlice";
+import { useEffect } from "react";
 
-export async function getStaticProps(){
-    const res = await fetch('http://37.32.21.146:8080/shop')
-    const data = await res.json();
-  
-    return {
-      props: {data}
-    }
-  
-  };
 
-  export default function Landing({data}: any) {
-    console.log('recive data',data);
+  export default function Landing() {
+
+    const dispatch = useAppDispatch();
+    const {categories, servicesForCategories} = useAppSelector((state) => state.foods);
+  
+    useEffect(() => {
+      dispatch(getFoods());
+    }, [dispatch]);
+  
+    console.log('categories:', categories);
+    console.log('servicesForCategories:', servicesForCategories);
     return (
         <Container>
           <MenuList />
-          <MenuCard categories={data.categories} />
+          <MenuCard categories={categories} />
           <ImageSlider />
-          <FoodList foods={data.foods} />
+          <FoodList foods={servicesForCategories} />
         </Container>
       );
     }
