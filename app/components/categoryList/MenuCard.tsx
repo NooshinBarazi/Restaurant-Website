4e5334e6b40da74fe86d73e4b896e1cfdba924ab
nooshinbarazi <nooshinbry@gmail.com>
2023-12-29 19:18:29 +0330

    manage multiple instance of swiper with useRef

---
+++ app/components/categoryList/MenuCard.tsx
@@ -0,0 +1,25 @@
+import { Tabs } from "@mui/material";
+import { MenuCardItem, CategoryProps } from "./MenuCardItem";
+
+interface Props {
+  categories: CategoryProps[];
+}
+
+export const MenuCard = ({ categories }: Props) => {
+  console.log("Categories in MenuCard:", categories);
+  return (
+    <Tabs scrollButtons="auto" variant="scrollable" value={0}>
+      {categories.map((category) => {
+        return (
+          <MenuCardItem
+            key={category.id}
+            id={category.id}
+            title={category.category_title}
+            icon={category.category_icon}
+            onClick={category.onClick}
+          />
+        );
+      })}
+    </Tabs>
+  );
+};
