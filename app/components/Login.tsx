1096bfdfbf5a71d725847317949ddfc0728cd3c4
nooshinbarazi <nooshinbry@gmail.com>
2024-01-01 22:04:56 +0330

    built login form

---
+++ app/components/Login.tsx
@@ -0,0 +1,10 @@
+import { Modal } from "@mui/material"
+import FormDialog from "./Modal"
+
+export const Login = () => {
+  return (
+   <FormDialog title={"خوش آمدید"} textButton={"ارسال کد"}>
+
+   </FormDialog>
+  )
+}
