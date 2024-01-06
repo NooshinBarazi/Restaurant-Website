acbdd867beecd9c586e1d025e79353bf130f093a
nooshinbarazi <nooshinbry@gmail.com>
2024-01-06 22:34:59 +0330

    add passwordfiled to textfiled

--- app/components/Login/LoginWithCode.tsx
+++ app/components/Login/LoginWithCode.tsx
@@ -1,9 +1,14 @@
-import { Button, InputAdornment, Typography } from "@mui/material";
-import VisibilityIcon from "@mui/icons-material/Visibility";
+import { Button, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { Input } from "../common/Input";
+import { useState } from "react";
+import { PasswordField } from "../common/PasswordField";
+import { FormButton } from "../common/FormButton";
+import { PasswordeRcovery } from "./PasswordeRcovery";
 
 export const LoginWithCode = () => {
+  const [visibility, setVisibility] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -20,7 +25,7 @@ export const LoginWithCode = () => {
     <>
       <Typography
         color="textSecondary"
-        sx={{ fontWeight: "700", fontSize: "1.6rem", textAlign: "center" }}
+        sx={{ fontWeight: "700", fontSize: "1.6rem", textAlign: "center"}}
       >
         برای ورود به حساب شماره موبایل و رمز عبور خود را وارد نمایید
       </Typography>
@@ -43,14 +48,15 @@ export const LoginWithCode = () => {
         <Input
           InputProps={{
             endAdornment: (
-              <InputAdornment position="end">
-                <VisibilityIcon />
-              </InputAdornment>
+              <PasswordField
+                visibility={visibility}
+                onClick={() => setVisibility(!visibility)}
+              />
             ),
           }}
           name="password"
           label="رمز ورود"
-          type="password"
+          type={visibility ? "text" : "password"}
           error={errors?.password?.message}
           register={register}
           disabled={false}
@@ -62,10 +68,11 @@ export const LoginWithCode = () => {
             },
           }}
         />
+        <FormButton label="رمز عبور را فراموش کردم" onClick={()=> console.log('clicked') }/>
         <Button
           type="submit"
           variant="contained"
-          sx={{ color: "#fff", margin: "1rem 0" }}
+          sx={{ color: "#fff", margin: "2rem 0" }}
           fullWidth
         >
           ورود به حساب
