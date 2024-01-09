8950a68ce609435e75a7709891f5512637e9d605
nooshinbarazi <nooshinbry@gmail.com>
2024-01-09 22:16:49 +0330

    change form template from modal to box

--- app/components/common/Modal.tsx
+++ app/components/common/Modal.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import { IconButton } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
+import { Box } from "@mui/material";
 
 interface DialogProps{
     title: string;
@@ -13,42 +9,15 @@ interface DialogProps{
 }
 
 export default function FormDialog({title, children}: DialogProps) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   return (
-    <React.Fragment>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog open={open} onClose={handleClose} fullWidth maxWidth = 'xs'>
-        <DialogActions>
-          <IconButton
-            onClick={handleClose}
-            aria-label="close"
-            sx={{
-              position: "absolute",
-              right: 8,
-              top: 8,
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </DialogActions>
-        <DialogTitle variant="h2" sx={{ margin: "2rem auto", fontWeight: '700'}}>
+      <Box sx={{textAlign: 'center', maxWidth: '55rem', backgroundColor: 'white', margin: 'auto'}}>
+        <DialogTitle color='primary' variant="h2" sx={{ margin: "2rem auto", fontWeight: '700'}}>
           {title}
         </DialogTitle>
-        <DialogContent sx={{paddingTop: '1rem !important'}}>
+        <DialogContent sx={{paddingTop: '1rem !important', textAlign: 'center'}}>
           {children}
         </DialogContent>
-      </Dialog>
-    </React.Fragment>
+      </Box>
   );
 }
